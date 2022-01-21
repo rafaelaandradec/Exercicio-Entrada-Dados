f787b9e1d04a33357f61bf0775fbd427fec3e0b6
rafaelaandradec <rafaelaandrade.costa@gmail.com>
2022-01-20 23:48:40 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos computadores tem na sua casa? ");
+            int computer = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do produto que deseja comprar: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite seu último nome, idade e altura (Mesma linha): ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimoNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(computer);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
